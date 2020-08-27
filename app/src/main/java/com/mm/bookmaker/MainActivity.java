6d5cc0df54d46d88f9bb7364555dacd1f028bc7e
Mateusz Kus <Gustaweek@users.noreply.github.com>
2020-08-27 13:02:54 +0200

    added money to bet

--- app/src/main/java/com/mm/bookmaker/MainActivity.java
+++ app/src/main/java/com/mm/bookmaker/MainActivity.java
@@ -7,26 +7,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mm.bookmaker.database.AppDatabase;
 import com.mm.bookmaker.models.Team;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     List<Team> team= new ArrayList<>();
+    TextView moneyToSpend;
+    CheckBox win1;
+    EditText moneyToBet;
+    CheckBox draw;
+    CheckBox win2;
+    Button betB;
+    float money = 500;
     private AppDatabase db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moneyToSpend= findViewById(R.id.moneyToSpendTV);
+        moneyToBet= findViewById(R.id.moneyToBet);
+        betB= findViewById(R.id.betB);
+        win1= findViewById(R.id.winFirstTeamCB);
+        draw= findViewById(R.id.drawCB);
+        win2= findViewById(R.id.winSecondTeamCB);
+
         team.add(new Team(1,"Legia Warszawa",37, 21,6,10));
-        team.add(new Team(2,"Lech Poznan",37, 18,12,7));
-        team.add(new Team(3,"Piast Gliwice",37, 18,7,12));
-        team.add(new Team(4,"Lechia Gdansk",37, 15,7,12));
+        team.add(new Team(2,"Lech Poznan",37, 18,7,12));
+        team.add(new Team(3,"Piast Gliwice",37, 18,12,7));
+        team.add(new Team(4,"Lechia Gdansk",37, 15,11,12));
         team.add(new Team(5,"Slask Wroclaw",37, 14,7,12));
         team.add(new Team(6,"Pogon Szczecin",37, 14,7,12));
         team.add(new Team(7,"Cracovia",37, 16,7,12));
@@ -39,11 +59,56 @@ public class MainActivity extends AppCompatActivity {
         team.add(new Team(14,"Arka Gdynia",37, 10,10,17));
         team.add(new Team(15,"Korona Kielce",37, 9,8,20));
         team.add(new Team(16,"LKS Lodz",37, 6,6,25));
-
+        moneyToSpend.setText(String.format(new Locale("PL"),"%.2f $",money));
+        //money();
 
 
         db = AppDatabase.getInstance(getApplicationContext());
     }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.winFirstTeamCB:
+                if (checked){
+
+                }
+
+            else
+
+                break;
+            case R.id.drawCB:
+                if (checked){
+
+                }
+
+            else
+
+                break;
+            case R.id.winSecondTeamCB:
+                if (checked){
+
+                }
+
+            else
+
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
+    public void setMoneyToBet(View view){
+        String betStr=moneyToBet.getText().toString();
+        if(betStr.isEmpty()){
+            return;
+        }
+        float bet= Float.valueOf(betStr.replace(",","."));
+        float result= money-bet;
+        moneyToSpend.setText(String.format(new Locale("PL"),"%.2f $",result));
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
