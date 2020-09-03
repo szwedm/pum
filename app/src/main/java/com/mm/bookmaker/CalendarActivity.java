c4d67973542dfecd37ca4445b1cc719abdecd713
Mateusz Kus <Gustaweek@users.noreply.github.com>
2020-09-03 18:22:38 +0200

    alpha version of calendar

--- app/src/main/java/com/mm/bookmaker/CalendarActivity.java
+++ app/src/main/java/com/mm/bookmaker/CalendarActivity.java
@@ -2,18 +2,42 @@ package com.mm.bookmaker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mm.bookmaker.database.AppDatabase;
+import com.mm.bookmaker.models.Match;
+
+import java.util.ArrayList;
+
 public class CalendarActivity extends AppCompatActivity {
+    private AppDatabase db;
+    CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        calendarView = findViewById(R.id.calendarView);
+        db = AppDatabase.getInstance(getApplicationContext());
+        db.matchDao().getAll();
+
+        ArrayList<Match> match = new ArrayList<>(db.matchDao().getAll());
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/"+ dayOfMonth ;
+                Log.d("ta", "onSelectedDayChange: yyyy/mm/dd:" + date);
+
+
+            }
+        });
 
     }
     @Override
