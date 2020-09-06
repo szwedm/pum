87ccc82ddc330839128bb81b59c7b285ee20c5dc
Mateusz Kus <Gustaweek@users.noreply.github.com>
2020-09-06 18:35:59 +0200

    visual changes

--- app/src/main/java/com/mm/bookmaker/LeagueTableActivity.java
+++ app/src/main/java/com/mm/bookmaker/LeagueTableActivity.java
@@ -34,37 +34,37 @@ public class LeagueTableActivity extends AppCompatActivity {
         tv0.setText( "#");
         tv0.setTextColor(Color.BLACK);
         tv0.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv0.setPadding(30,0,60,20);
+        tv0.setPadding(15,0,30,10);
         tbrow0.addView(tv0);
         TextView tv1 = new TextView(this);
         tv1.setText("Klub");
         tv1.setTextColor(Color.BLACK);
         tv1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv1.setPadding(60,0,60,20);
+        tv1.setPadding(30,0,30,10);
         tbrow0.addView(tv1);
         TextView tv2 = new TextView(this);
         tv2.setText("W");
         tv2.setTextColor(Color.BLACK);
         tv2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv2.setPadding(60,0,60,20);
+        tv2.setPadding(30,0,30,10);
         tbrow0.addView(tv2);
         TextView tv3 = new TextView(this);
         tv3.setText("R");
         tv3.setTextColor(Color.BLACK);
         tv3.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv3.setPadding(60,0,60,20);
+        tv3.setPadding(30,0,30,10);
         tbrow0.addView(tv3);
         TextView tv4 = new TextView(this);
         tv4.setText("P");
         tv4.setTextColor(Color.BLACK);
         tv4.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv4.setPadding(60,0,60,20);
+        tv4.setPadding(30,0,30,10);
         tbrow0.addView(tv4);
         TextView tv5 = new TextView(this);
         tv5.setText("PKT");
         tv5.setTextColor(Color.BLACK);
         tv5.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        tv5.setPadding(60,0,60,20);
+        tv5.setPadding(30,0,15,10);
         tbrow0.addView(tv5);
         stk.addView(tbrow0);
 
@@ -95,36 +95,36 @@ public class LeagueTableActivity extends AppCompatActivity {
             String number= String.valueOf(i+1);
             t1v.setText(number);
             t1v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t1v.setPadding(30,0,60,20);
+            t1v.setPadding(15,0,30,10);
             tbrow.addView(t1v);
 
             t2v.setText(teams.get(i).getName());
             t2v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t2v.setPadding(60,0,60,20);
+            t2v.setPadding(30,0,30,10);
             tbrow.addView(t2v);
 
             String wins= String.valueOf(teams.get(i).getWins());
             t3v.setText(wins);
             t3v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t3v.setPadding(60,0,60,20);
+            t3v.setPadding(30,0,30,10);
             tbrow.addView(t3v);
 
             String draws= String.valueOf(teams.get(i).getDraws());
             t4v.setText(draws);
             t4v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t4v.setPadding(60,0,60,20);
+            t4v.setPadding(30,0,30,10);
             tbrow.addView(t4v);
 
             String loses= String.valueOf(teams.get(i).getLoses());
             t5v.setText(loses);
             t5v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t5v.setPadding(60,0,60,20);
+            t5v.setPadding(30,0,30,10);
             tbrow.addView(t5v);
 
             String points= String.valueOf(teams.get(i).getPoints());
             t6v.setText(points);
             t6v.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            t6v.setPadding(60,0,30,20);
+            t6v.setPadding(30,0,15,10);
             tbrow.addView(t6v);
 
             stk.addView(tbrow);
